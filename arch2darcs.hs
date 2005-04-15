f2a4e1aee9e689fed403b9facf9c4138738b1709
John Goerzen <jgoerzen@complete.org>
2005-04-15 14:14:15 -0500

    Added main file
    darcs-hash:20050415191415-c2a52-a108c2d513fbaee8c7c5711f48d5735ce8611dd8.gz

---
+++ arch2darcs.hs
@@ -0,0 +1,60 @@
+{- 
+Copyright (C) 2005 John Goerzen <jgoerzen@complete.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+-}
+
+module Main where
+import System.Environment
+import System.Directory
+import MissingH.Logging.Logger
+import MissingH.IO
+import MissingH.Cmd
+
+usage = "Usage: arch2darcs\n" ++
+ "\n" ++
+ "You must be in your darcs working copy before running this command.\n" ++
+ "\n" ++
+ "Will apply any Arch patches, to the darcs repository in the current\n"++
+ "working directory, that are not\n" ++
+ "already in the current working directory.\n"
+
+getDarcsDir = 
+    do isdarcswc <- doesDirectoryExist "_darcs"
+       if isdarcswc
+          then getCurrentDirectory
+          else fail "CWD is not a Darcs directory"
+
+initLogging =
+    sequence_ $ map (\x -> updateGlobalLogger x (setLevel DEBUG))
+              ["arch2darcs", "MissingH.Cmd", "MissingH.Cmd.safeSystem",
+               "MissingH.Cmd.pOpen3", "MissingH.Cmd.pOpen",
+               "main"]
+
+info = infoM "main"
+
+getLines cmd args func = 
+    let f h = do c <- hGetLines h
+                 func c
+        in pOpen ReadFromPipe cmd args f
+
+main = do
+       initLogging
+       darcsdir <- getDarcsDir
+       info "Looking for new patches..."
+       getLines "tla" ["missing", "-f"] (mapM_ procPatch)
+
+procPatch patchname =
+    do putStrLn $ "Got patch " ++ patchname
