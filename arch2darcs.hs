9220d9b20dcfb76d57c99855c9d0ea4db0ac833c
John Goerzen <jgoerzen@complete.org>
2005-04-16 00:01:38 -0500

    Fixed indentation problem
    darcs-hash:20050416050138-c2a52-9c9dd58d1f04964fbe5e886e39df5271063cfbe9.gz

--- arch2darcs.hs
+++ arch2darcs.hs
@@ -110,14 +110,14 @@ darcsRename [src, dest] =
               then return ()
               else do f <- fileExist src
                       if f 
-                      -- If the source file exists, darcs mv gives an error because
-                      -- the dest file is already there.  Temporarily hide the
-                      -- source file from darcs mv so there's no error, then move it
-                      -- back.
-                      then do rename src tmpname
-                              darcsmv
-                              rename tmpname src
-                      else darcsmv
+                      -- If the source file exists, darcs mv gives an error 
+                      -- because the dest file is already there.  Temporarily 
+                      -- hide the source file from darcs mv so there's no
+                      -- error, then move it back.
+                         then do rename src tmpname
+                                 darcsmv
+                                 rename tmpname src
+                         else darcsmv
 darcsRename x = fail $ "Bad rename line in replay: " ++ show x
 
 record extraargs patchname loglines = 
